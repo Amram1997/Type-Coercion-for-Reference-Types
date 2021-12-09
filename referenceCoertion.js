b215861a0d5818f71df92d00fae58e21b275cbe3
Amram1997 <amramananyan9@gmail.com>
2021-12-09 23:19:02 +0400

    feat result unary operator

--- referenceCoertion.js
+++ referenceCoertion.js
@@ -44,4 +44,38 @@ console.log(5 + "hello")
 // 5hello
 console.log("hello" + NaN)
 // helloNaN
+// unary - operator
 console.log(-null)
+// -0
+console.log(-undefined)
+// NaN
+console.log(-true)
+// -1
+console.log(-false)
+// -0
+console.log(-5)
+// -5
+console.log(-"hello")
+// NaN
+console.log(-"234")
+// -234
+console.log(-"234.32e3")
+// -234320
+console.log( -new Number(NaN))
+// NaN
+console.log(-new String('hello'))
+// NaN
+console.log(-(/google.com/))
+// NaN
+console.log(-([]))
+// -0
+console.log(-[])
+// -0
+console.log(+[])
+// 0
+console.log(-[2])
+// -2
+console.log(-([2,3]))
+// NaN
+console.log(-(function(){}))
+// NaN
